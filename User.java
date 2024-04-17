433913459c28c0667daee69f8e0bbc8489794f8e
Peter Frangatzis <pfraggatzis2002@gmail.com>
2024-04-17 22:18:19 +0300

    merged: Accounts.java

--- User.java
+++ User.java
@@ -2,35 +2,66 @@
  * Dummy user class that is used by the Tracker remember registered users. 
  */
 public class User {
-	private String username, password;
-	private int count_dl = 0, count_fail = 0;
+	private String username;
+	private String password;
+	private int countDownloads;
+	private int countFailures;
 
-	User(String username, String password) {
+	// Constructor
+	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
+		this.countDownloads = 0;
+		this.countFailures = 0;
 	}
 
+	// Getters and setters for username and password
 	public String getUsername() {
-		return this.username;
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getPassword() {
-		return this.password;
+		return password;
 	}
 
-	public int getDownloads() {
-		return this.count_dl;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
-	public int getFailures() {
-		return this.count_fail;
+	// Getters and setters for countDownloads and countFailures
+	public int getCountDownloads() {
+		return countDownloads;
 	}
 
-	public synchronized void success() {
-		this.count_dl++;
+	public synchronized void setCountDownloads(int countDownloads) {
+		this.countDownloads = countDownloads;
 	}
 
-	public synchronized void failure() {
-		this.count_fail++;
+	public int getCountFailures() {
+		return countFailures;
+	}
+
+	public synchronized void setCountFailures(int countFailures) {
+		this.countFailures = countFailures;
+	}
+
+	// Method to increment countDownloads
+	public void incrementCountDownloads() {
+		this.countDownloads++;
+	}
+
+	// Method to increment countFailures
+	public void incrementCountFailures() {
+		this.countFailures++;
+	}
+
+	// Method to reset countDownloads and countFailures
+	public synchronized void resetCounts() {
+		this.countDownloads = 0;
+		this.countFailures = 0;
 	}
 }
